9762e949580b3993e6446ccdc6d8d50309c21810
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2016-03-10 15:11:33 -0800

    Initial commit

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2016 by Joao Rodrigues.  All rights reserved.
+# This code is part of the pydca distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
